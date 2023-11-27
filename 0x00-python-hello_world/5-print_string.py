f46f00a26f99a2c0f6ab2a357a0a17997959ed84
madutAjp <maduthuolin@gmail.com>
2023-11-28 00:43:53 +0300

    print my string 3 times

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     madut_str = "Holberton School"
-    print(madut_str * 3)
-    print(madut_str[:9])
+    print(madut_str * 3, end='\n\n' + madut_str[:9] + '\n')
