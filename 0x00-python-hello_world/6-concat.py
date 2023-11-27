58798db02dd59a0e433fa09f3ab641e1a4800b90
madutAjp <maduthuolin@gmail.com>
2023-11-28 01:53:08 +0300

    playwith strings

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    str1 = "Welcome to"
-    str2 = "Holberton School!"
-    print(str1 + " " + str2)
+str1 = "Welcome to"
+str2 = "Holberton School!"
+print(str1 + " " + str2)
