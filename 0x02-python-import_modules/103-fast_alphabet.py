f4e0823bcc45546b744833354b7050ec99cd4749
madutAjp <maduthuolin@gmail.com>
2023-12-02 22:04:18 +0300

    alphabet

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import string; print(getattr(string, 'ascii_uppercase'), end='')
+__import__('sys').stdout.write("ABCDEFGHIJKLMNOPQRSTUVWXYZ\n")
+
