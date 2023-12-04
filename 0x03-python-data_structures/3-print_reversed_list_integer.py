0457a9ca9cef0b626ade4dbde995ab429a5d0994
madutAjp <maduthuolin@gmail.com>
2023-12-04 14:55:57 +0300

    reversed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for madut in my_list[::-1]:
-        print("{}".format(madut))
+    for m in range (len(my_list) - 1, -1, -1):
+        print("{:d}".format(my_list[m]))
