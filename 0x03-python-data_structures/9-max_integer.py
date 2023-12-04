3ab2ebf14310a58314b870d85c830e1495adac36
madutAjp <maduthuolin@gmail.com>
2023-12-04 19:09:18 +0300

    max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
-        return None
-    max_num = my_list[0]
-    for num in my_list:
-        if num > max_num:
-            max_num = num
-            return max_num
+    if len(my_list) == 0:
+        return (None)
+    madut = my_list[0]
+    for m in range(len(my_list)):
+        if my_list[m] > madut:
+            madut = my_list[m]
+            return (madut)
