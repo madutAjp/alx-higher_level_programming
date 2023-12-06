21d9bb06ce5c0108a65f61ead3e483cf16f73301
madutAjp <maduthuolin@gmail.com>
2023-12-07 00:32:53 +0300

    roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
-        return 0
+        return None
     roman_dict = {'I': 1, 'V': 5, 'X':10, 'L':50, 'C':100, 'D': 500, 'M': 1000}
     total = 0
     for m in range(len(roman_string)):
