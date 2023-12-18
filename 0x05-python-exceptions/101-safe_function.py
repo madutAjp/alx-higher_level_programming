f72ad267e162280dc74950cd4dcec50b0ed5cec7
madutAjp <maduthuolin@gmail.com>
2023-12-18 23:40:50 +0300

    8safe

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
+
 from sys import stderr
+
 
 def safe_function(fct, *args):
     try:
         return fct(*args)
     except Exception as num:
-        print("Exception: {}\n".format(num))
+        stderr.write("Exception: {}\n".format(num))
         return None
