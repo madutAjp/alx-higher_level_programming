bcd64f6baf7a84a547be8e1c4c3ad0673cdf1086
madutAjp <maduthuolin@gmail.com>
2023-12-19 23:30:08 +0300

    4square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+
+"""class square that defines a square """
+
+
+class Square:
+    """class square that defines a square"""
+
+    def __init__(self, size=0):
+        """Iinstantiation with optional size"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """property to retrieve it"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """property setter to set it"""
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+    def area(self):
+        """public instance method that returns the current square area"""
+        return self.__size * self.__size
