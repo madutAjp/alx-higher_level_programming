cfd1613731d20ec2c8334f12e480f02280df7089
madutAjp <maduthuolin@gmail.com>
2023-12-19 22:05:25 +0300

    1square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""class square that defines a square"""
+
+
+class Square:
+    """class square that defines a square"""
+    def __init__(self, size):
+        self._size = size
