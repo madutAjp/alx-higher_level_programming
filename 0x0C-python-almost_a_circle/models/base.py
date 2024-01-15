0c56927a81dd30e3c7cb050b169ebdd2680a5882
madutAjp <maduthuolin@gmail.com>
2024-01-16 00:07:42 +0300

    madut

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Module for Base class."""
+'''Module for Base class.'''
 
 
 class Base:
-    """A representation of the base of our oop hierarchy."""
+    '''A representation of the base of our oop hierarchy.'''
 
     __nb_objects = 0
 
     def __init__(self, id-None):
-        """constructor."""
+        '''constructor.'''
         if id is not None:
             self.id = id
         else:
