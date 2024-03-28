98ea874233900eef1677246db8da000cfc8984ab
madutAjp <maduthuolin@gmail.com>
2024-03-29 01:53:17 +0300

    madutreq

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,8 +8,6 @@ sends a POST request to the passed URL with the email as a parameter,
 
 
  if __name__ == "__main__":
-     url = sys.arg[1]
-     value = {"email": sys.argv[2]}
-
-     r = requests.post(url, date=value)
+     payload = {"email": sys.argv[2]}
+     r = requests.post(url, date=payload)
      print(r.text)
