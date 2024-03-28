3e1f507a840e08eae7397c35087fa487d7283e45
madutAjp <maduthuolin@gmail.com>
2024-03-29 01:50:04 +0300

    madutreq

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+ and finally displays the body of the response.
+ """
+ import requests
+ import sys
+
+
+ if __name__ == "__main__":
+     url = sys.arg[1]
+     value = {"email": sys.argv[2]}
+
+     r = requests.post(url, date=value)
+     print(r.text)
