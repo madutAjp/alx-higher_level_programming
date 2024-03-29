a0e734622db85ff9be4e841efe362b728a2e4b3f
madutAjp <maduthuolin@gmail.com>
2024-03-29 03:42:39 +0300

    madutreq

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""A  script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id
+"""Uses the GitHub API to display a GitHub ID based on given credentials.
+
+Usage: ./10-my_github.py <GitHub username> <GitHub password>
+  - Uses Basic Authentication to access the ID.
 """
 import sys
 import requests
