d614ad4225553684ee230325c9814081077355dc
madutAjp <maduthuolin@gmail.com>
2024-03-29 03:41:09 +0300

    madutreq

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Uses the GitHub API to display a GitHub ID based on given credentials (username and password) and uses the GitHub API to display your id
+"""A  script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id
 """
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     r = requests.get("https://api.github.com/user", auth=auth)
     print(r.json().get("id"))
